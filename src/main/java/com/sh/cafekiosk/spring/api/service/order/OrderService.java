8696f7637941ff04fe76bbd744b7764c6b8d2c08
SunghoCha <tjdgh3725@gmail.com>
2024-07-06 19:43:47 +0900

    Test&TDD - Business Layer 테스트(4)
    * Service계층 리팩토링

--- src/main/java/com/sh/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/com/sh/cafekiosk/spring/api/service/order/OrderService.java
@@ -31,25 +31,32 @@ public class OrderService {
         List<String> productNumbers = orderCreateRequest.getProductNumbers();
         List<Product> products = findProductBy(productNumbers);
 
+        deductStockQuantities(products);
+
+        Order order = Order.create(products, registeredDateTime);
+        Order savedOrder = orderRepository.save(order);
+        return OrderCreateResponse.of(savedOrder);
+    }
+
+    /*
+        재고 감소 ~ 동시성 문제
+        * optimistic lock , pessimistic lock 등..
+     */
+    private void deductStockQuantities(List<Product> products) {
         //= 제약조건 (재고)
         // 재고 차감 체크가 필요한 상품들 필터링
-        List<String> stockProductNumbers = products.stream()
-                .filter(product -> ProductType.containsStockType(product.getType()))
-                .map(Product::getProductNumber)
-                .toList();
+        List<String> stockProductNumbers = extractStockProductNumbers(products);
 
-        // 재고 엔티티 조회 (* 리스트형태로 반복문 돌면 성능 떨어져서 맵으로 하는 이유 알아놓기 map이라고 표현한게 그냥 스트림 말한걸수도)
+        // 재고 엔티티 조회
         List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
-        Map<String, Stock> stockMap = stocks.stream()
-                .collect(Collectors.toMap(Stock::getProductNumber, stock -> stock));
 
         // 상품별 counting
-        Map<String, Long> productCountingMap = stockProductNumbers.stream()
-                .collect(Collectors.groupingBy(productNumber -> productNumber, Collectors.counting()));
+        Map<String, Long> productCountingMap = createCountingMapBy(stockProductNumbers);
 
         // 재고 차감 시도
         for (Stock stock : stocks) {
             int orderQuantity = productCountingMap.get(stock.getProductNumber()).intValue();
+
             if (stock.isQuantityLessThan(orderQuantity)) {
                 throw new IllegalArgumentException("재고가 부족한 상품이 있습니다."); // deductQuantity에서 검증하는데 여기서도 하는게 익숙하진 않음
             }
@@ -61,10 +68,6 @@ public class OrderService {
             서비스단과 도메인 내부의 예외 검증은 경우에 따라 다를 여지가 있으므로 같은 검증으로 중복된 것처럼 보이더라도 따로 할 수 있음
             *이건 상황에 따라 할수도 안할수도 있다는 뜻인건가? deductQuantity가 유효성 검증 책임을 가지는건 익숙한데 서비스에서 같은 검증하는건 상황봐서 생각해볼수도..
          */
-        
-        Order order = Order.create(products, registeredDateTime);
-        Order savedOrder = orderRepository.save(order);
-        return OrderCreateResponse.of(savedOrder);
     }
 
     private List<Product> findProductBy(List<String> productNumbers) {
@@ -73,7 +76,18 @@ public class OrderService {
                 .collect(Collectors.toMap(Product::getProductNumber, product -> product));
         return productNumbers.stream() // 이렇게 코딩하면 개수가 엄청나게 많은 경우는 어떻게 되는거지
                 .map(productMap::get)
-                .toList()
-                ;
+                .toList();
+    }
+
+    private static List<String> extractStockProductNumbers(List<Product> products) {
+        return products.stream()
+                .filter(product -> ProductType.containsStockType(product.getType()))
+                .map(Product::getProductNumber)
+                .toList();
+    }
+
+    private static Map<String, Long> createCountingMapBy(List<String> stockProductNumbers) {
+        return stockProductNumbers.stream()
+                .collect(Collectors.groupingBy(productNumber -> productNumber, Collectors.counting()));
     }
 }
